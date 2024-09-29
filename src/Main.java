c7aed7dfd0bfbd42df0ba701631976bc668bcf55
Vitalie Urmas <vitalie.urmas@gmail.com>
2024-09-29 18:00:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        long currentWorldPopulation = -7811080815L;
+        int carsProducedThisYear = -55235358;
+        char firstLetterInAlphabet = 'A';
+        boolean javaIsAmazing = true;
+        float pI = 3.14159F;
+
+        System.out.printf("Current World Population: " + currentWorldPopulation);
+        System.out.printf("\nCars Produced This Year: " + carsProducedThisYear);
+        System.out.printf("\nFirst Letter In Alphabet: " + firstLetterInAlphabet);
+        System.out.printf("\nJava Is Amazing: " + javaIsAmazing);
+        System.out.printf("\nPI: " + pI);
     }
 }
